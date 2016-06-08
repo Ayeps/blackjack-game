f25198a86020c3f944dae7de38dfc12fe6194954
Ayeps <ayeps2000@gmail.com>
2016-06-08 23:59:53 +0000

    initial

--- app.js
+++ app.js
@@ -60,7 +60,58 @@ controller.hears(['hello', 'hi'], 'message_received', function (bot, message) {
             bot.reply(message, 'Hello ' + user.name + '!!');
         } else {
             bot.reply(message, 'Hi, my name is Pepper and I am your Black Jack Dealer.!');
-            bot.startConversation(message, askName);
+            bot.startConversation(message, function (err, convo) {
+                if (!err) {
+                    convo.ask('What should I call you?', function (response, convo) {
+                        convo.ask('You want me to call you ' + response.text + '? (yes/no)', [{
+                            pattern: 'yes',
+                            callback: function (response, convo) {
+                                // since no further messages are queued after this,
+                                // the conversation will end naturally with status == 'completed'
+                                convo.next();
+                            }
+                        }, {
+                            pattern: 'no',
+                            callback: function (response, convo) {
+                                // stop the conversation. this will cause it to end with status == 'stopped'
+                                convo.stop();
+                            }
+                        }, {
+                            default: true,
+                            callback: function (response, convo) {
+                                convo.repeat();
+                                convo.next();
+                            }
+                        }]);
+
+                        convo.next();
+
+                    }, {
+                        'key': 'nickname'
+                    }); // store the results in a field called nickname
+
+                    convo.on('end', function (convo) {
+                        if (convo.status == 'completed') {
+
+                            controller.storage.users.get(message.user, function (err, user) {
+                                if (!user) {
+                                    user = {
+                                        id: message.user,
+                                    };
+                                }
+                                user.name = convo.extractResponse('nickname');
+
+                                controller.storage.users.save(user, function (err, id) {
+                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                                });
+                            });
+                        } else {
+                            // this happens if the conversation ended prematurely for some reason
+                            bot.reply(message, 'OK, nevermind!');
+                        }
+                    });
+                }
+            });
             bot.reply(message,
                 {
                     attachment: {
@@ -162,7 +213,6 @@ askName = function (response, convo) {
                 bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
             });
         });
-
         convo.say("Awesome.");
         convo.next();
     });
